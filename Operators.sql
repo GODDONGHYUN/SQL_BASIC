1524cbdc928ad52dc163ffdd5c7bfe537580cf1f
GODDONGHYUN <leenl1168@gmail.com>
2024-02-06 11:59:53 +0900

    2024-02-06 연산자 (산술 , 비교, 논리, LIKE, ORDER BY, DINTINCT)

--- Operators.sql
+++ Operators.sql
@@ -83,7 +83,7 @@ SELECT * FROM jeju WHERE direction_50m  NOT BETWEEN 270 AND 360;
 SELECT * FROM jeju WHERE observe_date IN ( '2023-07-01', '2023-08-01', '2023-09-01', '2023-10-01', '2023-11-01', '2023-12-01');
 SELECT * FROM jeju WHERE observe_date NOT IN ( '2023-07-01', '2023-08-01', '2023-09-01', '2023-10-01', '2023-11-01', '2023-12-01');
 
--- 논리연산자
+# 논리연산자
 -- AND 	(&&) : 좌항과 우항이 모두 true이면 true
 SELECT * FROM jeju WHERE speed_80m > 4 AND direction_50m < 180;
 
@@ -93,13 +93,32 @@ SELECT * FROM jeju WHERE speed_80m > 4 OR direction_50m < 180;
 -- XOR : 좌항과 우항이 다르면 true
 SELECT * FROM jeju WHERE speed_80m > 4 XOR direction_50m < 180;
 
+# LIKE 연산자 : 문자열을 비교할 때 패턴을 기준으로 비교
 
+-- % : 임의의 개수 : (0 ~ 무한대)의 문자
+-- 퍼센트의 위치를 보고 찾아줘야함 !
+SELECT * FROM jeju WHERE observe_date LIKE '20%';  -- 20으로 시작하는 문자열 조회
+SELECT * FROM jeju WHERE observe_date LIKE '%08';  -- 08로 끝나는 문자열 조회
+SELECT * FROM jeju WHERE observe_date LIKE '%08%'; -- 08이 들어가는 문자열 조회
 
+-- _ : 임의의 한 개 문자
+SELECT * FROM jeju WHERE observe_date LIKE '20_'; -- 20(세자리의 값) 조회 -> 결국은 해당하는 값이 없기때문에 안나옴
+SELECT * FROM jeju WHERE observe_date LIKE '2023-__-08'; -- 2023으로 시작하고 08로 끝나는 문자열 조회
 
+# 정렬 
+# ORDER BY : 쿼리 결과를 특정 기준에 따라 정렬
+-- ASC : 오름차순 정렬
+-- DESC : 내림차순 정렬
 
+SELECT * FROM jeju ORDER BY speed_80m ASC; -- speed_80m을 기준으로 작은 값으로 시작해서 오름차순으로 정렬
+SELECT * FROM jeju ORDER BY speed_80m DESC; -- speed_80m을 기준으로 제일 큰 값으로 시작해서 내림차순으로 정렬
+SELECT observe_date FROM jeju ORDER BY speed_80m DESC; -- 얘도 가능
 
+# 중복제거 
+# DISTINCT : SELECT 결과 테이블에서 컬럼의 조합 중복을 제거하여 출력
 
-
+SELECT DISTINCT above_avg_spd FROM jeju;
+SELECT DISTINCT above_avg_spd, above_avg_dir FROM jeju;
 
 
 
